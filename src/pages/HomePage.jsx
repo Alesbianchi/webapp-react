496b71c3470522ceba775b2436be87ef06059f59
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-03-12 15:37:18 +0100

    install and add axios get

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,27 @@
 //importo il componente moviecard
 import MovieCard from "./../components/MovieCard"
 
+//importo axios
+import axios from "axios"
+
 import { Link } from "react-router-dom"
 
 const HomePage = () => {
+
+    const fetchMovies = () => {
+        axios.get("http://localhost:3000/api/movies")
+            .then(
+                res => {
+                    console.log(res.data);
+
+                }
+            )
+            .catch(err => console.log(err)
+            )
+
+    }
+    fetchMovies();
+
     return (
         <>
             <h1 className="text-primary mt-3">BoolFlix</h1>
